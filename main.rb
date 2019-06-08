8476d13099a5eed6d072847b5ad7d774a194439b
Lena <sulyh9@gmail.com>
2019-06-08 18:16:02 +1000

    fix unnecessary use of instance variables

--- main.rb
+++ main.rb
@@ -15,8 +15,8 @@ get '/tickets' do
   @error = tickets['error_message']
 
   @first_page = 1
-  @range = 25
-  @last_page = @total / @range + 1 if @total % @range != 0
+  range = 25
+  @last_page = @total / range + 1 if @total % range != 0
   @current_page = params[:page].nil? ? @first_page : params[:page].to_i
   @previous_page = @current_page - 1 if @current_page != @first_page
   @next_page = @current_page + 1 if @current_page != @last_page
@@ -28,15 +28,15 @@ get '/tickets/:id' do
   @ticket = Ticket.find(@id.to_i)
   return status 404 if @ticket.nil?
 
-  @range = 25
-  @first_page = 1
-  @total = Ticket.fetch_tickets(page: params[:page])['total']
-  @last_page = @total / @range + 1 if @total % @range != 0
-  @page_hash = (@first_page..@last_page).each_with_object({}) do |page_number, hash|
+  range = 25
+  first_page = 1
+  total = Ticket.fetch_tickets(page: params[:page])['total']
+  last_page = total / range + 1 if total % range != 0
+  page_hash = (first_page..last_page).each_with_object({}) do |page_number, hash|
     tickets = Ticket.fetch_tickets(page: page_number, per_page: 25)
     hash[page_number] = tickets['tickets'].map(&:id)
   end
-  value = @page_hash.values.select { |number| number.include?(@id.to_i) }.first
-  @page = @page_hash.key(value)
+  value = page_hash.values.select { |number| number.include?(@id.to_i) }.first
+  @page = page_hash.key(value)
   erb :single_ticket
 end
